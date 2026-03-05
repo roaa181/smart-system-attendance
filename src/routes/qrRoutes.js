e7c3171709b027f9c4f6e5164d7e5c04a5af4f11
roaa181 <roaasamir181@gmail.com>
2026-03-05 23:06:18 +0200

    update files

--- src/routes/qrRoutes.js
+++ src/routes/qrRoutes.js
@@ -1,41 +1,28 @@
 import express from "express";
 import QRCode from "qrcode";
-// import crypto from "crypto";
 import Employee from "../models/Schema.Emp.js";
 import authMiddleware from "../middleware/authMiddle.js";
 
 const router = express.Router();
 
-// ─────────────────────────────────────────
-//   GET /api/qr/my-qr
-//   الموظف يشوف الـ QR بتاعه
-//   محتاج JWT token
-// ─────────────────────────────────────────
 router.get("/my-qr", authMiddleware, async (req, res) => {
   try {
     const employee = await Employee.findById(req.user._id);
 
-    // لو مفيش QR أو انتهى → ولّد واحد جديد
     if (!employee.qr_code || !employee.qr_expires || employee.qr_expires < new Date()) {
-      // employee.qr_code = employee.employeeNumber; // ← EMP0001 مثلاً
+      const expires = new Date();
+      expires.setHours(23, 59, 59, 999);
+
       employee.qr_code = employee.employeeNumber.slice(3); // 0028
+      employee.qr_expires = expires;
+      await employee.save();
+    }
 
-      // بيخلص الساعة 11:59 PM النهارده
-     if (!employee.qr_code || !employee.qr_expires || employee.qr_expires < new Date()) {
-  const expires = new Date();
-  expires.setHours(23, 59, 59, 999);
-
-  employee.qr_code = employee.employeeNumber.slice(3); // ← 0028
-  employee.qr_expires = expires;
-  await employee.save();
-}
-
-    // حوّل الـ token لصورة QR
     const qrImage = await QRCode.toDataURL(employee.qr_code);
 
     res.json({
-      token: employee.qr_code,
-      qrImage,                        // base64 صورة الـ QR للـ app
+      qr_code: employee.qr_code,
+      qrImage,
       expiresAt: employee.qr_expires,
     });
 
