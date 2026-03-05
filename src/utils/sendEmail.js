cc9ae9860b985af2c2d9a4110ce3a5bbeb9f42f1
roaa181 <roaasamir181@gmail.com>
2026-03-06 01:43:10 +0200

    update files

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,20 +1,53 @@
-import nodemailer from "nodemailer";
+ import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendOTPEmail = async (to, otp) => {
-  await transporter.sendMail({
-    from: `"Smart Attendance" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to,
     subject: "Password Reset OTP",
     text: `Your OTP code is: ${otp}`,
   });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// export const sendOTPEmail = async (to, otp) => {
+//   await transporter.sendMail({
+//     from: `"Smart Attendance" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject: "Password Reset OTP",
+//     text: `Your OTP code is: ${otp}`,
+//   });
+// };
