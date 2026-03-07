21146a78a067c7b2d372decd07b100aeefcba996
roaa181 <roaasamir181@gmail.com>
2026-03-07 11:11:43 +0200

    update files

--- src/services/auth.validation.js
+++ src/services/auth.validation.js
@@ -5,7 +5,7 @@ export let userValidation ={
     name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    role: Joi.string().valid("employee", "security").optional()
+    role: Joi.string().valid("employee", "security","admin").optional()
    }),
 };
 
