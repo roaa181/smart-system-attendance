a052ba179f26e45e87155de797e9f6ca75368e00
roaa181 <roaasamir181@gmail.com>
2026-03-29 02:34:03 +0200

    update files

--- src/models/DeviceCommand.js
+++ src/models/DeviceCommand.js
@@ -1,22 +1,22 @@
-// models/DeviceCommand.js
-import mongoose from "mongoose";
+// // models/DeviceCommand.js
+// import mongoose from "mongoose";
 
-const deviceCommandSchema = new mongoose.Schema({
-  command: {
-    type: String,
-    enum: ["open_gate", "close_gate", "open_camera", "close_camera"],
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "done"],
-    default: "pending",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+// const deviceCommandSchema = new mongoose.Schema({
+//   command: {
+//     type: String,
+//     enum: ["open_gate", "close_gate", "open_camera", "close_camera"],
+//     required: true,
+//   },
+//   status: {
+//     type: String,
+//     enum: ["pending", "done"],
+//     default: "pending",
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
 
-const DeviceCommand = mongoose.model("DeviceCommand", deviceCommandSchema);
-export default DeviceCommand;
+// const DeviceCommand = mongoose.model("DeviceCommand", deviceCommandSchema);
+// export default DeviceCommand;
