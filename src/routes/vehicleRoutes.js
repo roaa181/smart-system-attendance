435f926fae08f490449302828aef68a411d288d4
roaa181 <roaasamir181@gmail.com>
2026-04-11 13:35:05 +0200

    update files

--- src/routes/vehicleRoutes.js
+++ src/routes/vehicleRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
 import {registerVehicle} from "../controllers/vehicleController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+router.post("/register", authMiddleware, registerVehicle);
 
 
-router.post("/register", registerVehicle);
+
 
 export default router;
